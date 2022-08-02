8bf0fd9ee08a04df1236333c4763b9e8a445b5a7
nihar kushwaha <niharkushwaha7@gmail.com>
2022-08-02 17:33:25 +0530

    categories update

--- components/Categories.js
+++ components/Categories.js
@@ -3,14 +3,14 @@ import React from "react";
 
 export default function Categories({ data }) {
   return (
-    <div className="sm:px-24 h-20 flex items-center justify-center bg-[#DDDDDD]">
-      <div className=" flex gap-4 overflow-x-scroll p-3">
+    <div className="sm:px-24 p-3 h-auto flex items-center justify-center ">
+      <div className=" flex gap-4 overflow-x-scroll p-3 no-scrollbar ">
         {data
           .filter((element, idx) => element.category)
           .map((element, idx) => {
             return (
               <button
-                className="p-3 cursor-pointer bg-[#222831] text-gray-400 rounded-md hover:bg-[#F05454]"
+                className="p-3 cursor-pointer ring-1 ring-[#F05454] shadow-md shadow-[#f1d9d9] bg-white rounded-md hover:bg-[#F05454] font-medium"
                 key={"option-" + idx}
                 onClick={() => Router.push(`/?category=${element.category}`)}
               >
--- components/View.js
+++ components/View.js
@@ -3,7 +3,7 @@ import ViewCard from "./ViewCard";
 
 export default function View({ movies }) {
   return (
-    <div className="px-5 sm:px-20 my-10 grid md:grid-cols-3 xl:grid-cols-4 3xl:flex flex-wrap justify-center gap-10 md:gap-10 xl:gap-20">
+    <div className="px-5 sm:px-20 mb-10 grid md:grid-cols-3 xl:grid-cols-4 3xl:flex flex-wrap justify-center gap-10 md:gap-10 xl:gap-20">
       {movies
         .filter((movie, idx) => idx < 14 && idx > 1)
         .map((movie) => {
